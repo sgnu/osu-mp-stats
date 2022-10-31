8e4c7143680f0551ddca5063d3988c33a184fc84
sgnu <sam.nguyen.1223@gmail.com>
2022-10-31 16:19:52 -0400

    Add inputting mp links to load data from osu! api

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,31 @@ export default {
   },
   data() {
     return {
-      game: exampleGame,
-      players: examplePlayers,
-      beatmaps: exampleBeatmaps,
+      game: {},
+      players: {},
+      beatmaps: {},
       teamOne: [],
       teamTwo: [],
       fetchQueue: [],
+      userInput: '',
+      mpId: -1,
     }
   },
   created() {
     // this.getUniquePlayers()
     // this.getBeatmapsInfo()
-    this.calculateMatchCosts()
+    // this.calculateMatchCosts()
   },
   methods: {
     getUniquePlayers() {
       const tempArray = []
 
-      exampleGame.games.forEach(game => {
+      this.game.games.forEach(game => {
         game.scores.forEach(score => {
           if (tempArray.indexOf(score.user_id) === -1) {
+            if (!this.players[score.user_id])  {
+              this.players[score.user_id] = {}
+            }
             tempArray.push(score.user_id)
           }
         })
@@ -41,6 +46,8 @@ export default {
         this.fetchQueue.push(`get_user-${id}`)
         setTimeout(this.addPlayer, 50 * index, id)
       })
+
+      this.calculateMatchCosts()
     },
 
     getBeatmapsInfo() {
@@ -63,13 +70,13 @@ export default {
 
     calculateMatchCosts() { // calculate using osuPlus's formula: https://i.imgur.com/BJPOKDY.png
       const tempPlayers = {}
-      Object.values(this.players).forEach(player => {
-        tempPlayers[player.user_id] = {
-          user_id: player.user_id,
-          gameSum: 0,
-          gamesPlayed: 0
-        }
-      })
+      // Object.values(this.players).forEach(player => {
+      //   tempPlayers[player.user_id] = {
+      //     user_id: player.user_id,
+      //     gameSum: 0,
+      //     gamesPlayed: 0
+      //   }
+      // })
 
       // parse through all played games
       this.game.games.forEach(game => {
@@ -80,13 +87,20 @@ export default {
         game.scores.forEach(score => {
           const points = parseInt(score.score)
           if (points !== 0) {
+            if (!tempPlayers[score.user_id]) {
+              tempPlayers[score.user_id] = {
+                user_id: score.user_id,
+                gameSum: 0,
+                gamesPlayed: 0
+              }
+            }
             totalScore += points
             count++
             players[score.user_id] = {
               user_id: score.user_id,
               score: points,
             }
-            this.players[score.user_id].team = score.team
+            this.players[score.user_id]['team'] = score.team
           }
         })
 
@@ -140,25 +154,62 @@ export default {
 
     async addPlayer(id) {
       const params = { 'type': 'id', 'u': id }
-      this.players[id] = (await apiCall('get_user', params))[0]
+      const playerData =  (await apiCall('get_user', params))[0]
+      Object.keys(playerData).forEach(key => {
+        this.players[id][key] = playerData[key]
+      })
 
       const index = this.fetchQueue.indexOf(`get_user-${id}`)
       if (index > -1) {
         this.fetchQueue.splice(index, 1)
       }
+    },
+
+    handleSubmit() {
+      if (parseInt(this.userInput)) {
+        this.mpId = parseInt(this.userInput)
+      } else if (this.userInput.includes('osu.ppy.sh/community/matches')) {
+        const regex = new RegExp(/\d+/)
+        this.mpId = parseInt((this.userInput.match(regex))[0])
+      } else {
+        this.mpId = -1
+        window.alert("That didn't appear to be a valid match")
+      }
+
+      if (this.mpId > 0) {
+        this.fetchQueue.push(`get_match-${this.mpId}`)
+        this.players = {}
+        this.beatmaps = {}
+        this.teamOne = []
+        this.teamTwo = []
+        const params = { 'mp': this.mpId }
+        apiCall('get_match', params).then(data => {
+          if (data.games && data.games.length > 0) {  // found a valid mp link
+            this.game = data
+            this.getUniquePlayers()
+            this.getBeatmapsInfo()
+            // this.calculateMatchCosts()
+            this.fetchQueue.splice(this.fetchQueue.indexOf(`get_match-${this.mpId}`), 1)
+          } else {
+            window.alert("That mp link wasn't valid!")
+            this.fetchQueue.splice(this.fetchQueue.indexOf(`get_match-${this.mpId}`), 1)
+          }
+        })
+      }
     }
   },
   computed: {
     isLoading() {
-      return this.fetchQueue.length !== 0
+      return this.fetchQueue.length > 0
     }
   }
 }
 </script>
 
 <template>
+  <div class="user-input"><input type="text" v-model="userInput" @keyup.enter="handleSubmit"/> <button @click="handleSubmit">Enter</button></div>
   <h1 v-if="isLoading">Loading...</h1>
-  <div class="app" v-else>
+  <div class="app" v-if="!isLoading && Object.keys(game).length > 0">
     <h1>{{ game.match.name }}</h1>
     <div class="team">
       <Player v-for="player in teamOne" :player="player" />
@@ -174,9 +225,52 @@ export default {
 </template>
 
 <style scoped>
+.user-input {
+  background-color: var(--ctp-mocha-base);
+  border-radius: 8px;
+
+  overflow: hidden;
+  width: 420px;
+  height: 32px;
+  position: absolute;
+  left: 50%;
+  translate: -50%;
+}
+
+.user-input input {
+  background: none;
+  border: none;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: var(--ctp-mocha-text);
+  outline: none;
+
+  padding: 0 12px;
+  width: 356px;
+  height: 32px;
+}
+
+.user-input button {
+  font-family: 'Noto Sans JP';
+  background: none;
+  border: none;
+  box-sizing: border-box;
+  color: var(--ctp-mocha-blue);
+
+  height: 32px;
+  width: 60px;
+  transition: 0.167s ease-out;
+}
+
+.user-input button:hover {
+  background-color: var(--ctp-mocha-blue);
+  color: var(--ctp-mocha-base);
+}
+
 .app {
   display: grid;
   grid-template-columns: 1fr 1fr;
+  margin-top: 64px;
 }
 
 .team {
